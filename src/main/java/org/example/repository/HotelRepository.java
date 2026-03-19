7a73acc20c7b44053682785e883f7d5920b6c283
Krolb <pivamaks@gmail.com>
2026-03-19 23:27:28 +0300

    entity: добавление repository

---
+++ src/main/java/org/example/repository/HotelRepository.java
@@ -0,0 +1,10 @@
+package org.example.repository;
+
+import org.example.entity.Hotel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface HotelRepository extends JpaRepository<Hotel, Long> {
+
+}
